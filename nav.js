93ba613477817ed82e988f77f92f2fe4a3e427a8
Eric Sheng <44331935+ericsheng495@users.noreply.github.com>
2022-08-04 22:43:52 -0400

    Add files via upload

--- nav.js
+++ nav.js
@@ -4,10 +4,12 @@ const navToggle = document.querySelector(".mobile-nav-toggle");
 navToggle.addEventListener("click", ()=>{
   const visibility = primaryNav.getAttribute("data-visible");
   if(visibility === "false"){
-    //css not working!!!
     primaryNav.setAttribute("data-visible", true);
+    navToggle.setAttribute("aria-expanded", true);
+    
   }else{
     primaryNav.setAttribute("data-visible", false);
+    navToggle.setAttribute("aria-expanded", false);
   }
   console.log(visibility);
 
